050c0fe2095d91676d0b9a5b7754650591c8a4d0
Jordan Amos <jordan.amos@justice.qld.gov.au>
2023-05-29 15:33:04 +1000

    Add source

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# synapse-to-ipynb
-Manage and synchronize Synapse notebooks with Jupyter notebooks.
+# Synapse-to-IPython Notebook (synapse-to-ipynb)
+
+[Azure Synapse Analytics](https://azure.microsoft.com/en-au/products/synapse-analytics/) uses notebooks for various computational tasks however managing these in a local dev environment and with proper version control is difficult. If this is a problem you have run into, this tool might be for you.
+
+This program was designed primarily for use in a CI/CD pipeline.
+
+
+## Features
+
+- Create new IPython notebooks from Synapse notebooks
+- Delete IPython notebooks that don't exist in Synapse notebooks
+- Update Synapse notebooks from IPython notebooks
+
+## Installation
+
+```
+pip install git+https://github.com/jordanamos/synapse-to-ipynb
+```
+
+## Usage
+
+1. Create ipynbs from Synapse notebooks. The source Synapse directory is considered the point of truth and so the default behaviour is that if the target already contains .ipynb files, ones that don't have a matching Synapse notebook will be deleted:
+
+    ```
+    synapse-to-ipynb --source <synapse_directory> --target <ipython_directory>
+    ```
+
+    - `--source`: The source directory containing Synapse notebooks (.json).
+    - `--target`: The target directory to store IPython Notebooks (.ipynb).
+
+2. Develop the .ipynb files in your faviourite IDE
+
+3. To migrate the changes back to Synapse use the `--update` flag:
+    ```
+    synapse-to-ipynb --source <synapse_directory> --target <ipython_directory> --update
+    ```
+    - `--source`: The source directory containing Synapse notebooks (.json).
+    - `--target`: The target directory to store IPython Notebooks (.ipynb).
+    - `--update`: Put the tool in update mode
+
+## Contributing
+
+FeedBback and contributions are welcome! If you encounter any issues or have suggestions for improvement, please create an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ synapse_to_ipynb.py
@@ -0,0 +1,242 @@
+from __future__ import annotations
+
+import argparse
+import json
+import logging
+import os
+import shutil
+import tempfile
+from collections.abc import Sequence
+from pathlib import Path
+from typing import Any
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+
+class NotebookDirectoryManager:
+    # https://ipython.org/ipython-doc/3/notebook/nbformat.html
+    _DEFAULT_IPYNB = {
+        "nbformat": 4,
+        "nbformat_minor": 0,
+        "metadata": {},
+        "cells": [],
+    }
+
+    def __init__(self, synapse_dir: Path, ipynb_dir: Path) -> None:
+        if not synapse_dir.is_dir():
+            raise NotADirectoryError(f"'{synapse_dir}' is not a valid directory")
+        if not ipynb_dir.is_dir():
+            raise NotADirectoryError(f"'{ipynb_dir}' is not a valid directory")
+
+        self.synapse_dir = synapse_dir.resolve(strict=True)
+        self.ipynb_dir = ipynb_dir.resolve(strict=True)
+
+        self.synapse_nbs = sorted(self.synapse_dir.glob("*.json"), key=lambda f: f.stem)
+        self.ipynbs = sorted(self.ipynb_dir.glob("**/*.ipynb"), key=lambda f: f.stem)
+
+        self.synapse_only_nbs = self._diff_files(self.synapse_nbs, self.ipynbs)
+        self.ipynb_only_nbs = self._diff_files(self.ipynbs, self.synapse_nbs)
+
+    @staticmethod
+    def _diff_files(left_files: list[Path], right_files: list[Path]) -> list[Path]:
+        """
+        :return:
+            A list of file paths in :left_files: where the filename (minus extension)
+            is not present in :right_files:
+        :rtype: list[Path]
+        """
+        right_stems = [f.stem for f in right_files]
+        return [path for path in left_files if path.stem not in right_stems]
+
+    @staticmethod
+    def update_synapse_notebook_from_ipynb(synnb_path: Path, ipynb_path: Path) -> None:
+        """
+        Update the 'cells' contents of a Synapse notebook with the contents
+        of a Jupyter notebook (.ipynb).
+
+        :param synnb_path: The Synapse notebook to update.
+        :type synnb_path: str
+        :param ipynb_path: The source ipynb to update the Synapse notebook from.
+        :type ipynb_path: str
+
+        :return: 0 if the notebook was updated successfully.
+        :rtype: int
+        """
+
+        # Read the ipynb src that we will use to update the synapse notebook with
+        with open(ipynb_path) as nb:
+            ipynb_cells = json.load(nb)["cells"]
+
+        # Create a temporary notebook file
+        fd, tmp_notebook_path = tempfile.mkstemp(dir=synnb_path.parent)
+        try:
+            # Copy the contents of the actual notebook file to the temporary one
+            shutil.copyfile(synnb_path, tmp_notebook_path)
+
+            # Update the temporary synpase notebook file's 'cells'
+            # property with the ipynb src we read earlier
+            with open(fd, "r+") as f:
+                synnb_json = json.load(f)
+                synnb_json["properties"]["cells"] = ipynb_cells
+                f.seek(0)
+                f.truncate()
+                json.dump(synnb_json, f, indent=4)
+
+            # Finally, replace the synpase notebook with the updated temporary one
+            os.replace(tmp_notebook_path, synnb_path)
+        except BaseException:
+            os.remove(tmp_notebook_path)
+            raise
+
+    def create_ipynb_from_synapse_nb(self, synapse_nb: Path) -> Path:
+        """
+        Creates a Jupyter Notebook (.ipynb) from a Synapse Notebook.
+
+        :return: The Path to the the newly created ipynb file
+        :raises: KeyError: if the 'properties' key does not exist
+        """
+
+        # Read in the synapse notebook.
+        with open(synapse_nb) as nb:
+            # We're only interested in the 'properties' key.
+            synapse_notebook: dict[str, Any] = json.load(nb)["properties"]
+
+        # Update the default data with the data from the synapse notebook
+        ipynb_data = {}
+        ipynb_data.update(
+            {
+                key: synapse_notebook.get(key, default)
+                for key, default in self._DEFAULT_IPYNB.items()
+            }
+        )
+
+        # Synapse doesnt create the 'metadata' key by default.
+        # It's required so we'll create it here, if it doesnt exist.
+        cells: list[dict[str, Any]] = ipynb_data["cells"]
+        for cell in cells:
+            cell.setdefault("metadata", {})
+
+        # Create the new .ipynb file in the target directory.
+        ipynb_filename = f"{synapse_nb.stem}.ipynb"
+        ipynb_path = self.ipynb_dir / ipynb_filename
+        with open(ipynb_path, "w") as ipynb:
+            json.dump(ipynb_data, ipynb, indent=4)
+
+        return ipynb_path
+
+
+def update_synapse_nbs(manager: NotebookDirectoryManager) -> int:
+    # These should be the same, but let's check
+    synapse_nbs_stems = [f.stem for f in manager.synapse_nbs]
+    ipynbs_stems = [f.stem for f in manager.ipynbs]
+    if synapse_nbs_stems != ipynbs_stems:
+        logger.error(f"File names in {synapse_nbs_stems} don't match {ipynbs_stems}")
+        return 1
+
+    ret = 0
+    for synnb, ipynb in zip(manager.synapse_nbs, manager.ipynbs, strict=True):
+        try:
+            manager.update_synapse_notebook_from_ipynb(synnb, ipynb)
+        except (KeyError, json.JSONDecodeError) as e:
+            logger.error(f"Unable to update Synapse notebook '{synnb.name}': {e}")
+            ret |= 1
+        else:
+            logger.info(f"Successfully updated '{synnb.name}' from '{ipynb.name}'")
+            ret |= 0
+    return ret
+
+
+def delete_files(files: list[Path]) -> int:
+    ret = 0
+    for f in files:
+        try:
+            f.unlink()
+        except FileNotFoundError as e:
+            logger.error(f"Unable to delete {f}: {e}")
+            ret |= 1
+        else:
+            logger.info(f"Deleted {f.name}")
+            ret |= 0
+    return ret
+
+
+def create_synapse_only_nbs(manager: NotebookDirectoryManager) -> int:
+    ret = 0
+    for synapse_nb in manager.synapse_only_nbs:
+        try:
+            ipynb_path = manager.create_ipynb_from_synapse_nb(synapse_nb)
+        except (KeyError, json.JSONDecodeError) as e:
+            _msg = f"Unable to create ipynb from '{synapse_nb.name}': %s"
+            logger.error(_msg % (f"Missing key: {e}" if isinstance(e, KeyError) else e))
+            ret |= 1
+        else:
+            ret |= 0
+            logger.info(f"Created '{ipynb_path.name}' from '{synapse_nb.name}'")
+    return ret
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser(
+        description=(
+            "Create or Delete Jupyter notebooks from Synapse notebooks. "
+            "If the '--update' flag is set, update existing Synapse notebooks "
+            "from Jupyter notebooks"
+        )
+    )
+    parser.add_argument(
+        "--source",
+        help="The source directory containing Synapse notebooks (.json)",
+        type=Path,
+        metavar="DIR",
+        required=True,
+    )
+    parser.add_argument(
+        "--target",
+        help="The target directory containing Jupyter notebooks (.ipynb)",
+        type=Path,
+        metavar="DIR",
+        required=True,
+    )
+    parser.add_argument(
+        "--update",
+        help=(
+            "Set this flag to instead update the Synapse notebooks in the "
+            "source directory with the contents of the Jupyter notebooks "
+            "in the target directory"
+        ),
+        action="store_true",
+    )
+
+    args = parser.parse_args(argv)
+    manager = NotebookDirectoryManager(args.source, args.target)
+    ret = 0
+
+    if args.update:
+        # Handle Modified notebooks
+        if manager.synapse_only_nbs or manager.ipynb_only_nbs:
+            _new = [f.name for f in manager.ipynb_only_nbs]
+            _del = [f.name for f in manager.synapse_only_nbs]
+            logger.error(
+                f"New {_new} or deleted {_del} notebooks have been detected. "
+                "Please create, delete or rename notebooks directly in Synapse."
+            )
+            return 1
+        ret = update_synapse_nbs(manager)
+    else:
+        # Handle New / Deleted / Renamed notebooks
+        if not (manager.synapse_only_nbs or manager.ipynb_only_nbs):
+            logger.error("Nothing to create or delete.")
+            return 1
+        ret = create_synapse_only_nbs(manager)
+        ret |= delete_files(manager.ipynb_only_nbs)
+
+    return ret
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
